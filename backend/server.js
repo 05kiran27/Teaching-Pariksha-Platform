cae053a2ef12858ec0c3f773ea28cab4ba298743
05kiran27 <147203223+05kiran27@users.noreply.github.com>
2025-10-05 23:02:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -96,11 +96,10 @@ app.post("/detect", upload.single("image"), async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
